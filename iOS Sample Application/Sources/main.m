b26fa56f25ef959ece42ada32d5597f9ccbef252
Markus Gasser <markus.gasser@swisscom.com>
2013-03-03 23:27:24 +0100

    Shorter sample app name

--- iOS Sample Application/Sources/main.m
+++ iOS Sample Application/Sources/main.m
@@ -7,11 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
-
 #import "DNDAppDelegate.h"
 
-int main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([DNDAppDelegate class]));
     }
